fc3e7f482e34f04d97b53d6dd7fdf5047766ff96
Lukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2016-06-04 18:45:38 +0200

    Initial commit

---
+++ WpfApplication1/Tab.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+
+namespace WpfApplication1
+{
+    public class Tab : BindableBase
+    {
+        public Tab(string name, string text)
+        {
+            this.name = name;
+            this.text = text;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+    }
+}
---
+++ WpfApplication1/TabGroup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace WpfApplication1
+{
+    public class TabGroup : BindableBase
+    {
+        private Random random;
+
+        public TabGroup()
+        {
+            this.random = new Random();
+            this.addNewTabCommand = new Lazy<DelegateCommand>(() => new DelegateCommand(AddNewTab, () => true));
+            this.otherCommand = new Lazy<DelegateCommand>(() => new DelegateCommand(Method, () => Selected != null).ObservesProperty(() => Selected));
+            Tabs.CollectionChanged += TabsChanged;
+        }
+
+
+        private void Method()
+        {
+
+        }
+
+        private void TabsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var newItems = e.NewItems?.Cast<Tab>().ToList();
+            if (newItems?.Any() == true)
+            {
+                Selected = newItems.Last();
+            }
+        }
+
+        private void AddNewTab()
+        {
+            Tabs.Add(new Tab(GetNextName(), GetRandomContent()));
+        }
+
+        private string GetRandomContent()
+        {
+            return random.Next().ToString();
+        }
+
+        private int num = 0;
+        private string GetNextName() => $"{num++}";
+
+        private Tab selected;
+        public Tab Selected
+        {
+            get { return selected; }
+            set { SetProperty(ref selected, value); }
+        }
+
+        public ObservableCollection<Tab> Tabs { get; } = new ObservableCollection<Tab>();
+
+
+        private readonly Lazy<DelegateCommand> addNewTabCommand;
+        public DelegateCommand AddNewTabCommand => addNewTabCommand.Value;
+
+        private readonly Lazy<DelegateCommand> otherCommand;
+        public DelegateCommand OtherCommand => otherCommand.Value;
+    }
+}
